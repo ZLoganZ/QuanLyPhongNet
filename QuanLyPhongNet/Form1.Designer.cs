570900e2363e0016e3125eea97c8dabdd5698e42
taynguyen0206 <phamtrinhtaynguyen2002@gmail.com>
2022-05-08 23:55:42 +0700

    commit nam2

--- QuanLyPhongNet/Form1.Designer.cs
+++ QuanLyPhongNet/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -54,18 +55,28 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(359, 287);
+            this.button3.Location = new System.Drawing.Point(522, 202);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(345, 84);
             this.button3.TabIndex = 2;
             this.button3.Text = "cu nam ơi là cu nam";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(225, 321);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(379, 84);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Nam ơi là nam ơii";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -81,6 +92,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
